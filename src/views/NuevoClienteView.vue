e530c131d0fa67e4b34f8049e0360cb9999ee298
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-28 00:26:19 -0400

    Leer datos del Formulario

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -8,6 +8,10 @@
             type: String,
         }
     })
+
+    const handleSubmit = (data) => {
+        console.log(data)
+    }
 </script>
 
 <template>
@@ -25,10 +29,12 @@
                     type="form"
                     submit-label="Agregar Cliente"
                     :incomplete-message=false
+                    @submit="handleSubmit"
                 > 
                     <FormKit
                         type="text"
                         label="Nombre"
+                        name="nombre"
                         placeholder="Nombre del cliente"
                         validation="required"
                         :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
@@ -37,6 +43,7 @@
                     <FormKit
                         type="text"
                         label="Apellido"
+                        name="apellido"
                         placeholder="Apellido del cliente"
                         validation="required"
                         :validation-messages="{required: 'El Apellido del Cliente es Obligatorio'}"
@@ -45,6 +52,7 @@
                     <FormKit
                         type="email"
                         label="Email"
+                        name="email"
                         placeholder="Email del cliente"
                         validation="required|email"
                         :validation-messages="{required: 'El Email del Cliente es Obligatorio', email: 'Coloca un email valido'}"
@@ -53,6 +61,7 @@
                     <FormKit
                         type="text"
                         label="Teléfono"
+                        name="telefono"
                         placeholder="Teléfono: +XX XXX-XXX-XXX"
                         validation="?matches:/^[+][0-9]{2} [0-9]{3}-[0-9]{3}-[0-9]{4}$/"
                         :validation-messages="{matches: 'El Formato no es valido'}"
@@ -61,12 +70,14 @@
                     <FormKit
                         type="text"
                         label="Empresa"
+                        name="empresa"
                         placeholder="Empresa del cliente"
                     />
 
                     <FormKit
                         type="text"
                         label="Puesto"
+                        name="puesto"
                         placeholder="Puesto del cliente"
                     />
                 </FormKit>
