e252c67e4d3ac4059827c306cf7f9c0bb64bce44
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-30 23:38:59 -0400

    Obtener data para editar cliente

---
+++ src/views/EditarClienteView.vue
@@ -0,0 +1,110 @@
+<script setup>
+    // import {reactive} from 'vue'
+    import { onMounted } from "vue";
+    import ClienteService from "../services/ClienteService";
+    import { FormKit } from '@formkit/vue'
+    import {useRouter, useRoute} from 'vue-router'
+    import RouterLink from "../components/UI/RouterLink.vue"
+    import Heading from "../components/UI/Heading.vue"
+
+    const route = useRoute()
+    const router = useRouter()
+
+    const { id } = route.params
+    console.log(id);
+
+    onMounted(() => {
+        ClienteService.obtenerCliente(id)
+            .then(({data}) => console.log(data))
+            .catch((error) => console.log('Sucedio un error: ', error))
+    })
+
+    defineProps({
+        titulo:{
+            type: String,
+        }
+    })
+
+    const handleSubmit = (data) => {
+
+    }
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="listado-clientes">
+                regresar
+            </RouterLink>
+        </div>
+        <Heading>{{titulo}}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-4/5 py-20 px-6">
+                <FormKit
+                    type="form"
+                    submit-label="Agregar Cliente"
+                    :incomplete-message=false
+                    @submit="handleSubmit"
+                > 
+                    <!-- :value="formData" -->
+                    <FormKit
+                        type="text"
+                        label="Nombre"
+                        name="nombre"
+                        placeholder="Nombre del cliente"
+                        validation="required"
+                        :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
+                    />
+
+                    <FormKit
+                        type="text"
+                        label="Apellido"
+                        name="apellido"
+                        placeholder="Apellido del cliente"
+                        validation="required"
+                        :validation-messages="{required: 'El Apellido del Cliente es Obligatorio'}"
+                    />
+
+                    <FormKit
+                        type="email"
+                        label="Email"
+                        name="email"
+                        placeholder="Email del cliente"
+                        validation="required|email"
+                        :validation-messages="{required: 'El Email del Cliente es Obligatorio', email: 'Coloca un email valido'}"
+                    />    
+
+                    <FormKit
+                        type="text"
+                        label="Teléfono"
+                        name="telefono"
+                        placeholder="Teléfono: XXX-XXX-XXX"
+                        validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{matches: 'El Formato no es valido'}"
+                    />  
+                    
+                    <FormKit
+                        type="text"
+                        label="Empresa"
+                        name="empresa"
+                        placeholder="Empresa del cliente"
+                    />
+
+                    <FormKit
+                        type="text"
+                        label="Puesto"
+                        name="puesto"
+                        placeholder="Puesto del cliente"
+                    />
+                </FormKit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .formkit-wrapper{
+        max-width: 100%;
+    }
+</style>
